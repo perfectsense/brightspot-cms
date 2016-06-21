1463a3f3e0a092167807a3f3053bf4f073b8d960
Michael Likouris <mlikouris@perfectsensedigital.com>
2016-06-21 12:30:20 -0400

    BSP-2189, rte table remove &nbsp; from table when editing

--- tool-ui/src/main/webapp/script/v3/input/richtext2.js
+++ tool-ui/src/main/webapp/script/v3/input/richtext2.js
@@ -3975,7 +3975,7 @@ define(['jquery', 'v3/input/richtextCodeMirror', 'v3/input/tableEditor', 'v3/plu
             // Set a flag so we only update the table cell if user clicks the save button
             self.tableEditCancel = false;
             
-            value = $el.html();
+            value = $el.html().replace(/&nbsp;/g, '');
 
             self.$tableEditDiv.popup('source', $el);
             self.$tableEditDiv.popup('open');
