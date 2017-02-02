da6bfad145f6981ab8f7dfdc7a27dadee4cd0e2e
Patrick Fitzgerald <pat@perfectsensedigital.com>
2017-02-02 15:35:53 -0500

    BSP-2980 fix drag and drop for popups and tabs
    Modify the drag and drop code so it will work within a popup. Also do not display drag and drop regions if the element is hidden (such as in a tabbed ui).

--- tool-ui/src/main/webapp/script/v3.js
+++ tool-ui/src/main/webapp/script/v3.js
@@ -414,6 +414,35 @@ function() {
 
   })();
 
+  // Add function to calculate z-index
+  $.fn.zIndex = function (zIndex) {
+    if (zIndex !== undefined) {
+      return this.css("zIndex", zIndex);
+    }
+
+    if (this.length) {
+      var elem = $(this[ 0 ]), position, value;
+      while (elem.length && elem[ 0 ] !== document) {
+        // Ignore z-index if position is set to a value where z-index is ignored by the browser
+        // This makes behavior of this function consistent across browsers
+        // WebKit always returns auto if the element is positioned
+        position = elem.css("position");
+        if (position === "absolute" || position === "relative" || position === "fixed") {
+          // IE returns 0 when zIndex is not specified
+          // other browsers return a string
+          // we ignore the case of nested elements with an explicit value of 0
+          // <div style="z-index: -10;"><div style="z-index: 0;"></div></div>
+          value = parseInt(elem.css("zIndex"), 10);
+          if (!isNaN(value) && value !== 0) {
+            return value;
+          }
+        }
+        elem = elem.parent();
+      }
+    }
+    return 0;
+  };
+
   // Handle file uploads from drag-and-drop.
   (function() {
     var docEntered;
@@ -478,6 +507,13 @@ function() {
             $fileSelectorInput,
             $fileSelectorLabel;
 
+        // Only add drag and drop if the element is visible.
+        // For example, an element might be hidden in another tab,
+        // so we do not want drag and drop region for that element.
+        if (!$(this).is(':visible')) {
+          return;
+        }
+
         // Check if this is a FileSelector widget, such as used to upload a single new file
         if ($(this).is('.fileSelector')) {
           $fileSelector = $(this);
@@ -492,7 +528,11 @@ function() {
         overlayCss = $.extend($container.offset(), {
           'height': $container.outerHeight(),
           'position': 'absolute',
-          'width': $container.outerWidth()
+          'width': $container.outerWidth(),
+           // Calculate the z-index for the element and use the same value for the drag-and-drop overlay.
+           // This should ensure that things like popups will show the drag and drop overlay,
+           // and still be on top of other drag-and-drop overlays that are in a lower stacking context.
+          'z-index': $(this).zIndex() || 'auto'
         });
 
         $dropZone = $('<div/>', {
