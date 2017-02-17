cb17bfe3deaa3e08cb360a45876281f9b91c0b03
Dan Slaughter <dan@perfectsensedigital.com>
2017-02-17 17:20:04 -0500

    Adds ManualView (name subject to change).
    Marker interface for ViewModels that do not want to take part in automatic ViewBinding discovery.

---
+++ db/src/main/java/com/psddev/cms/view/ManualView.java
@@ -0,0 +1,8 @@
+package com.psddev.cms.view;
+
+/**
+ * Marker interface for {@link ViewModel} classes that do not want to take part
+ * in automatic {@link ViewBinding} discovery.
+ */
+public interface ManualView {
+}
