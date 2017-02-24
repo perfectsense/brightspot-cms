cd333fb10ac7b02d790c1bf92ccb6c0530ceb741
Dan Slaughter <dan@perfectsensedigital.com>
2017-02-24 14:35:43 -0500

    Makes ToolEntity Notifiable.

--- db/src/main/java/com/psddev/cms/db/ToolEntity.java
+++ db/src/main/java/com/psddev/cms/db/ToolEntity.java
@@ -1,8 +1,9 @@
 package com.psddev.cms.db;
 
+import com.psddev.cms.notification.Notifiable;
 import com.psddev.dari.db.Recordable;
 
-public interface ToolEntity extends Global, Recordable {
+public interface ToolEntity extends Global, Notifiable, Recordable {
 
     /**
      * Returns all tool users that are represented by this entity.
