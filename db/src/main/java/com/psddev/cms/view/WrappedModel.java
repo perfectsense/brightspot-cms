5ec753abb4ed90b83d038993d982aa9430a82b33
Dan Slaughter <dan@perfectsensedigital.com>
2017-03-14 10:45:55 -0400

    Adds WrappedModel to simplify ViewModel impls. calling createView.

---
+++ db/src/main/java/com/psddev/cms/view/WrappedModel.java
@@ -0,0 +1,19 @@
+package com.psddev.cms.view;
+
+/**
+ * Indicates an object that is wrapping another. Classes implementing this
+ * interface are detected during calls to {@link ViewModel#createView(Class, Object)}
+ * and {@link ViewModel#createView(String, Object)} and will recursively have
+ * their {@link #unwrap()} method called to obtain the actual model object used
+ * for {@linkplain ViewModel} / {@linkplain ViewBinding} look up. This API
+ * serves as a convenience to {@linkplain ViewModel} implementations to simplify
+ * their invocations of {@code createView} when dealing with models that merely
+ * delegate to others.
+ */
+public interface WrappedModel {
+
+    /**
+     * @return The object being wrapped.
+     */
+    Object unwrap();
+}
