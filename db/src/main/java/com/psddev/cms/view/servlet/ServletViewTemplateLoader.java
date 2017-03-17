83a64883fe001d5c60f6e07d2fca622244304a79
Hyoo Lim <hyoolim@gmail.com>
2017-03-17 13:24:41 -0400

    Changes ServletViewTemplateLoader instances to be cached.

--- db/src/main/java/com/psddev/cms/view/servlet/ServletViewTemplateLoader.java
+++ db/src/main/java/com/psddev/cms/view/servlet/ServletViewTemplateLoader.java
@@ -3,9 +3,14 @@ package com.psddev.cms.view.servlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Objects;
 
 import javax.servlet.ServletContext;
 
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.psddev.cms.view.UrlViewTemplateLoader;
 import com.psddev.dari.util.CodeUtils;
 
@@ -16,12 +21,42 @@ public class ServletViewTemplateLoader extends UrlViewTemplateLoader {
 
     private static final String TEMPLATE_NOT_FOUND_MESSAGE_FORMAT = "Could not find template at path [%s]!";
 
+    private static LoadingCache<ServletContext, ServletViewTemplateLoader> INSTANCES = CacheBuilder
+            .newBuilder()
+            .weakKeys()
+            .build(new CacheLoader<ServletContext, ServletViewTemplateLoader>() {
+
+                @Override
+                @SuppressWarnings("deprecation")
+                public ServletViewTemplateLoader load(ServletContext servletContext) {
+                    return new ServletViewTemplateLoader(servletContext);
+                }
+            });
+
     private ServletContext servletContext;
 
     /**
-     * @param servletContext the servlet HTTP servlet context .
+     * Returns an instance that loads templates from the given
+     * {@code servletContext}.
+     *
+     * @param servletContext Nonnull.
+     * @return Nonnull.
      */
+    public static ServletViewTemplateLoader getInstance(ServletContext servletContext) {
+        Preconditions.checkNotNull(servletContext);
+        return INSTANCES.getUnchecked(servletContext);
+    }
+
+    /**
+     * Creates an instance that loads templates from the given
+     * {@code servletContext}.
+     *
+     * @param servletContext Nonnull.
+     * @deprecated Use {@link #getInstance(ServletContext)} instead.
+     */
+    @Deprecated
     public ServletViewTemplateLoader(ServletContext servletContext) {
+        Preconditions.checkNotNull(servletContext);
         this.servletContext = servletContext;
     }
 
@@ -41,5 +76,17 @@ public class ServletViewTemplateLoader extends UrlViewTemplateLoader {
             throw new IOException(String.format(TEMPLATE_NOT_FOUND_MESSAGE_FORMAT, path));
         }
         return templateUrl;
+    }
+
+    @Override
+    public int hashCode() {
+        return servletContext.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other
+                || (other instanceof ServletViewTemplateLoader
+                && Objects.equals(servletContext, ((ServletViewTemplateLoader) other).servletContext));
     }
 }
