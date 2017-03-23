a99463249426f51c452528652fed31e4ae0b3982
Dan Slaughter <dan@perfectsensedigital.com>
2017-03-23 18:16:49 -0400

    Moves all notify logic into the Publisher. Removes the async API. Adds several hooks so sub-classes can influence how notifications are sent and to whom.

--- db/src/main/java/com/psddev/cms/notification/Publisher.java
+++ db/src/main/java/com/psddev/cms/notification/Publisher.java
@@ -1,5 +1,10 @@
 package com.psddev.cms.notification;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.psddev.dari.db.ObjectType;
 import com.psddev.dari.db.Query;
 
 /**
@@ -27,6 +32,39 @@ public abstract class Publisher<S extends Subscription<C>, C> {
     protected abstract Class<S> getSubscriptionType();
 
     /**
+     * Sub-classes may override this method to filter the types of Receivers
+     * eligible to receive this notification.
+     *
+     * @return The type of receivers to notify. Default is ALL types.
+     */
+    protected Class<? extends Receiver> getReceiverType() {
+        return Receiver.class;
+    }
+
+    /**
+     * Sub-classes may override this method to alter the query used to fetch
+     * potential receivers.
+     *
+     * @return The query used to fetch all potential receivers.
+     */
+    protected Query<? extends Receiver> getReceiversQuery() {
+
+        Query<? extends Receiver> receiverQuery = Query.from(getReceiverType());
+
+        Class<S> subscriptionTypeClass = getSubscriptionType();
+
+        if (subscriptionTypeClass != null) {
+            ObjectType subscriptionType = ObjectType.getInstance(subscriptionTypeClass);
+
+            if (subscriptionType != null) {
+                receiverQuery.where(ReceiverData.SUBSCRIPTION_TYPES_FIELD + " = ?", subscriptionType);
+            }
+        }
+
+        return receiverQuery;
+    }
+
+    /**
      * Gets the potential receivers to be notified when the subscription is
      * published. The default implementation queries for all Receiver instances
      * in the database. Sub-classes may override this method to provide a
@@ -35,36 +73,51 @@ public abstract class Publisher<S extends Subscription<C>, C> {
      * @return The receivers that should be notified.
      */
     protected Iterable<? extends Receiver> getReceivers() {
-        return Query.from(Receiver.class).iterable(0);
+        return getReceiversQuery().iterable(0);
     }
 
     /**
-     * Publishes the subscription with the given {@code context} by notifying
-     * all registered receivers as determined by {@link #getReceivers()}.
-     * This method blocks until all of the notifications are sent.
+     * Notifies this receiver about a particular subscription.
+     *
+     * @param subscriptionClass The type of subscription to notify on.
+     * @param context Contextual data about the subscription.
      */
-    public final void publishNotification() {
+    protected final void notifyReceiver(Receiver receiver, Class<S> subscriptionClass, C context) {
 
-        for (Receiver receiver : getReceivers()) {
-            receiver.as(ReceiverData.class).notify(getSubscriptionType(), context);
+        ReceiverData receiverData = receiver.as(ReceiverData.class);
+
+        List<DeliveryOption> receiverDeliveryOptions = receiverData.getDeliveryOptions();
+
+        // Get the subscriptions matching the given the subscription class type.
+        List<Subscription<?>> subscriptions = receiverData.getSubscriptions().stream()
+                .filter(s -> subscriptionClass.isAssignableFrom(s.getClass()))
+                .collect(Collectors.toList());
+
+        for (Subscription<?> subscription : subscriptions) {
+
+            @SuppressWarnings("unchecked")
+            Notification<C> notification = new Notification<>((S) subscription, receiver, context);
+
+            Set<ObjectType> deliveryOptionTypes = subscription.getDeliveryOptionTypes();
+
+            // Get the delivery options from the subscription that match those configured by the receiver.
+            List<DeliveryOption> deliveryOptions = receiverDeliveryOptions.stream()
+                    .filter(rdo -> deliveryOptionTypes.contains(rdo.getState().getType()))
+                    .collect(Collectors.toList());
+
+            for (DeliveryOption deliveryOption : deliveryOptions) {
+                deliveryOption.deliver(notification);
+            }
         }
     }
 
     /**
      * Publishes the subscription with the given {@code context} by notifying
      * all registered receivers as determined by {@link #getReceivers()}.
-     * The notifications are sent asynchronously and this method will return
-     * immediately.
      */
-    public final void publishNotificationEventually() {
-
-        // TODO: Use AsyncQueue for high volume scenarios rather than spawning a new Thread each time.
-        Thread sendNotification = new Thread() {
-            @Override
-            public void run() {
-                publishNotification();
-            }
-        };
-        sendNotification.start();
+    public void publishNotification() {
+        for (Receiver receiver : getReceivers()) {
+            notifyReceiver(receiver, getSubscriptionType(), context);
+        }
     }
 }
--- db/src/main/java/com/psddev/cms/notification/ReceiverData.java
+++ db/src/main/java/com/psddev/cms/notification/ReceiverData.java
@@ -24,6 +24,8 @@ public class ReceiverData extends Modification<Receiver> {
 
     public static final String SUBSCRIPTIONS_FIELD = FIELD_PREFIX + "subscriptions";
 
+    public static final String SUBSCRIPTION_TYPES_FIELD = FIELD_PREFIX + "getSubscriptionTypes";
+
     @ToolUi.Tab(TAB_NAME)
     @ToolUi.Heading("Delivery Preferences")
     @ToolUi.Unlabeled
@@ -81,38 +83,11 @@ public class ReceiverData extends Modification<Receiver> {
         this.subscriptions = subscriptions;
     }
 
-    /**
-     * Notifies this receiver about a particular subscription.
-     *
-     * @param subscriptionClass The type of subscription to notify on.
-     * @param context Contextual data about the subscription.
-     * @param <S> The type of subscription.
-     * @param <C> The type of contextual data for the subscription.
-     */
-    public final <S extends Subscription<C>, C> void notify(Class<S> subscriptionClass, C context) {
-
-        List<DeliveryOption> receiverDeliveryOptions = getDeliveryOptions();
-
-        // Get the subscriptions matching the given the subscription class type.
-        List<Subscription<?>> subscriptions = getSubscriptions().stream()
-                .filter(s -> subscriptionClass.isAssignableFrom(s.getClass()))
-                .collect(Collectors.toList());
-
-        for (Subscription<?> subscription : subscriptions) {
-
-            @SuppressWarnings("unchecked")
-            Notification<C> notification = new Notification<>((S) subscription, getOriginalObject(), context);
-
-            Set<ObjectType> deliveryOptionTypes = subscription.getDeliveryOptionTypes();
-
-            // Get the delivery options from the subscription that match those configured by the receiver.
-            List<DeliveryOption> deliveryOptions = receiverDeliveryOptions.stream()
-                    .filter(rdo -> deliveryOptionTypes.contains(rdo.getState().getType()))
-                    .collect(Collectors.toList());
-
-            for (DeliveryOption deliveryOption : deliveryOptions) {
-                deliveryOption.deliver(notification);
-            }
-        }
+    @Indexed
+    @ToolUi.Hidden
+    public Set<ObjectType> getSubscriptionTypes() {
+        return getSubscriptions().stream()
+                .map(s -> s.getState().getType())
+                .collect(Collectors.toSet());
     }
 }
