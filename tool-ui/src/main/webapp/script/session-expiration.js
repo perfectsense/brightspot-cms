56a3741149868cc99eb1c655c8042de9a22c9f4a
Xhama Vyas <xhama@perfectsensedigital.com>
2017-11-10 16:13:04 -0500

    Refactors logic outside of setInterval() wherever possible.

--- tool-ui/src/main/webapp/script/session-expiration.js
+++ tool-ui/src/main/webapp/script/session-expiration.js
@@ -8,52 +8,54 @@ function($, bsp_utils) {
         var warnMins = 5;
         var expireTime = new Date(Date.parse($('meta[name=bsp\\.tu\\.sessionExpiration]').attr('content')));
         var warnTime = new Date((expireTime.valueOf()+(-1000*60*warnMins)));
-        var goTime = Math.max(0, warnTime.getTime() - Date.now())
+        var goTime = Math.max(0, warnTime.getTime() - Date.now());
+        var warnTime = Math.max(0, expireTime.getTime() - Date.now());
 
         setTimeout(function() {
-            setInterval(function() {
-                var time = new Date(Date.now());
-                var message = '';
-                if (time > expireTime) {
-                    message = "You have been logged out and your work may not be saved."
-                } else {
-                    // construct warning message and redirect url
+            // construct warning message and redirect url
+            var expireMillis = Math.max(0, expireTime.getTime() - Date.now());
+            var message = '';
 
+            var url = window.location.href;
+            if (url.indexOf('?') < 0) {
+                url += "?_renewSession=true";
+            } else {
+                url += "&_renewSession=true";
+            }
+
+            if (!($(".widget-logIn")[0])) {
+                var span = $("<span>").attr("name", "logout-message");
+
+                if ($("body").hasClass("hasToolBroadcast"))	{
+                    if (!($("span[name=logout-message]")[0])) {
+                        $(".toolBroadcast").append(" - ").append(span);
+                    }
+                } else {
+                    $("body").addClass("hasToolBroadcast");
+                    var broadcastElem = $("<div>").addClass("toolBroadcast");
+                    broadcastElem.append(span);
+                    $("body").prepend(broadcastElem);
+                }
+            }
+
+            setInterval(function() {
+                expireMillis -= 1000;
+
+                if (expireMillis <= 0) {
+                    message = "You have been logged out and your work may not be saved.";
+                } else {
                     // calculate time left.
-                    var timeLeft = new Date(expireTime.valueOf() - time.valueOf());
+                    var timeLeft = new Date(expireMillis);
                     var mins = timeLeft.getMinutes();
                     var secs = timeLeft.getSeconds();
-
-                    var url = window.location.href;
-                    if (url.indexOf('?') < 0) {
-                        url += "?_renewSession=true";
-                    } else {
-                        url += "&_renewSession=true";
-                    }
 
                     message = ' You will be logged out in '
                         + (("0" + mins).slice(-2)) + ':'
                         + (("0" + secs).slice(-2)) + ', <a href="'
                         + url + '">I\'m still working.</a>';
                 }
-                // render message if not already redirected to login
-                if (!($(".widget-logIn")[0])) {
-                    var span = $("<span>").attr("name", "logout-message")
-                                          .html(message);
 
-                    if ($("body").hasClass("hasToolBroadcast"))	{
-                        if (!($("span[name=logout-message]")[0])) {
-                            $(".toolBroadcast").append(" - ").append(span);
-                        } else {
-                            $('span[name=logout-message]').html(message);
-                        }
-                    } else {
-                        $("body").addClass("hasToolBroadcast");
-                        var broadcastElem = $("<div>").addClass("toolBroadcast");
-                        broadcastElem.append(span);
-                        $("body").prepend(broadcastElem);
-                    }
-                }
+                $("span[name=logout-message]").html(message);
 
             }, 1000);
         }, goTime);
