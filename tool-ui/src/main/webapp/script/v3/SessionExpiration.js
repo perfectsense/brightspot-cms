e34a214e2f16c367c36ca773fe21c479e5f0da62
Xhama Vyas <xhama@perfectsensedigital.com>
2018-03-21 16:31:24 -0400

    Makes countdown times consistent across windows and further stylistic changes.

--- tool-ui/src/main/webapp/script/v3/SessionExpiration.js
+++ tool-ui/src/main/webapp/script/v3/SessionExpiration.js
@@ -5,68 +5,89 @@ define([
     function($, bsp_utils) {
         bsp_utils.onDomInsert(document, 'meta[name=bsp-tu-sessionExpiration]', {
             insert: function () {
-                var warnMins = 5;
-                var expireTime = new Date(Date.parse($('meta[name=bsp-tu-sessionExpiration]').attr('content')));
-                var warnTime = new Date((expireTime.valueOf() + (-1000 * 60 * warnMins)));
-                var goTime = Math.max(0, warnTime.getTime() - Date.now());
+                var warnMins = 5
+                var expireTime = new Date(getLatestSessionExpiration())
+                var expireMillis = Math.max(0, expireTime.getTime() - Date.now())
+                var goTime = Math.max(0, expireMillis + (-1000 * 60 * warnMins))
 
                 setTimeout(function() {
                     // construct warning message and redirect url
-                    var expireMillis = Math.max(0, expireTime.getTime() - Date.now());
-                    var message = '';
-                    var timeLeft = new Date(expireMillis);
+                    var expireMillis = Math.max(0, expireTime.getTime() - Date.now())
+                    var message = ''
+                    var timeLeft = new Date(expireMillis)
                     if (expireMillis == 0) {
-                        message = "You have been logged out and your work may not be saved.";
+                        message = "You have been logged out and your work may not be saved."
                     } else {
                         message = ' You will be logged out in '
                                       + (("0" + timeLeft.getMinutes()).slice(-2)) + ':'
                                       + (("0" + timeLeft.getSeconds()).slice(-2)) + ', <a href="'
-                                      + url + '">I\'m still working.</a>';
+                                      + url + '">I\'m still working.</a>'
                     }
 
-                    var url = window.location.href;
+                    var url = window.location.href
                     if (url.indexOf('?') < 0) {
-                        url += "?_renewSession=true";
+                        url += "?_renewSession=true"
                     } else {
-                        url += "&_renewSession=true";
+                        url += "&_renewSession=true"
                     }
 
                     if ($(".widget-logIn").size() == 0) {
                         var span = $("<span>").attr("name", "logout-message")
-                                              .html(message);
+                                              .html(message)
 
                         if ($("body").hasClass("hasToolBroadcast"))	{
                             if ($("span[name=logout-message]").size() == 0) {
-                                $(".toolBroadcast").append(" - ").append(span);
+                                $(".toolBroadcast").append(" - ").append(span)
                             }
                         } else {
-                            $("body").addClass("hasToolBroadcast");
-                            var broadcastElem = $("<div>").addClass("toolBroadcast");
-                            broadcastElem.append(span);
-                            $("body").prepend(broadcastElem);
+                            $("body").addClass("hasToolBroadcast")
+                            var broadcastElem = $("<div>").addClass("toolBroadcast")
+                            broadcastElem.append(span)
+                            $("body").prepend(broadcastElem)
                         }
                     }
 
                     var warningInterval = setInterval(function() {
-                        expireMillis -= 1000;
+                        expireTime = new Date(getLatestSessionExpiration(expireTime))
+                        var expireMillis = Math.max(0, expireTime.getTime() - Date.now())
 
                         if (expireMillis <= 0) {
-                            message = "You have been logged out and your work may not be saved.";
-                            clearInterval(warningInterval);
+                            message = "You have been logged out and your work may not be saved."
+                            clearInterval(warningInterval)
                         } else {
                             // calculate time left.
-                            timeLeft = new Date(expireMillis);
+                            timeLeft = new Date(expireMillis)
 
                             message = ' You will be logged out in '
                                 + (("0" + timeLeft.getMinutes()).slice(-2)) + ':'
                                 + (("0" + timeLeft.getSeconds()).slice(-2)) + ', <a href="'
-                                + url + '">I\'m still working.</a>';
+                                + url + '">I\'m still working.</a>'
                         }
 
-                        $("span[name=logout-message]").html(message);
+                        $("span[name=logout-message]").html(message)
 
-                    }, 1000);
-                }, goTime);
+                    }, 1000)
+                }, goTime)
+
+                function getLatestSessionExpiration (expireTime) {
+                    if (!expireTime) {
+                        var expireTime = new Date(Date.parse($('meta[name=bsp-tu-sessionExpiration]').attr('content')))
+                    } else {
+                        expireTime = new Date(expireTime)
+                    }
+
+                    var maxSessionExpiration = new Date(localStorage.getItem("sessionExpiration"))
+                    // if local storage not set or needs updating
+                    if (!maxSessionExpiration || expireTime > maxSessionExpiration) {
+                        localStorage.setItem("sessionExpiration", expireTime)
+                    // if local storage has been updated in another window update here
+                    } else if (maxSessionExpiration > expireTime) {
+                        expireTime = maxSessionExpiration
+                    }
+
+                    return expireTime
+                }
+
             }
-        });
-    });
+        })
+    })
